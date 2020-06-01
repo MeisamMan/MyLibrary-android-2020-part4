e70e7a72ded2b1cdf3856e74e7d0ffdb7ad2cf78
Meisam <Meisam@meiCode.org>
2020-06-02 01:39:30 +0400

    The source code for the vide called 'Create Book Activity' in the Android App Development Master Class for 2020

---
+++ app/src/main/java/org/meicode/mylibrary/AllBooksActivity.java
@@ -0,0 +1,36 @@
+package org.meicode.mylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class AllBooksActivity extends AppCompatActivity {
+
+    private RecyclerView booksRecView;
+    private BookRecViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_books);
+
+        adapter = new BookRecViewAdapter(this);
+        booksRecView = findViewById(R.id.booksRecView);
+
+        booksRecView.setAdapter(adapter);
+        booksRecView.setLayoutManager(new LinearLayoutManager(this));
+
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book(1, "1Q84", "Haruki Murakami", 1350, "https://images-na.ssl-images-amazon.com/images/I/41FdmYnaNuL._SX322_BO1,204,203,200_.jpg",
+                "A work of maddening brilliance", "Long Description"));
+        books.add(new Book(2, "The Myth of Sisyphus", "Albert Camus", 250, "https://miro.medium.com/max/500/1*DDsOx6D3oe8ZxcA-OTfIDA.jpeg",
+                "One of the most influential works of this century, this is a crucial exposition of existentialist thought.",
+                "Long Description"));
+        adapter.setBooks(books);
+    }
+}
